15dc871e940e4f0361688acd02d860988333c110
Ariel Barrios <ariel29111978@hotmail.com>
2023-12-18 16:06:08 -0300

    Agrega la funcionalidad para enviar mensajes del cliente al servidor y del servidor al cliente

--- public/js/chat.js
+++ public/js/chat.js
@@ -4,3 +4,39 @@ if (!username) {
   throw new Error('Username is required');
 }
 const socket = io();
+
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+const chatElement = document.querySelector('#chat');
+
+const renderMessage = (payload) => {
+  const { userId, message } = payload;
+
+  //Crear un div element
+  const divElement = document.createElement('div');
+
+  //Agregar la clase message
+  divElement.classList.add('message');
+
+  //Si el id no es el mio, agregar "incognit"
+  if (userId !== socket.id) {
+    divElement.classList.add('incognit');
+  }
+
+  //Agregar el mensaje al div
+  divElement.innerHTML = message;
+
+  //Agregar el div al chat
+  chatElement.appendChild(divElement);
+};
+
+socket.on('on-message', renderMessage);
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const message = input.value;
+  input.value = '';
+
+  socket.emit('send-message', message);
+});
--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,4 +1,10 @@
-import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import {
+  ConnectedSocket,
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
 import { ChatService } from './chat.service';
 import { Server, Socket } from 'socket.io';
 import { OnModuleInit } from '@nestjs/common';
@@ -11,8 +17,22 @@ export class ChatGateway implements OnModuleInit {
   constructor(private readonly chatService: ChatService) {}
 
   onModuleInit() {
-    this.server.on('connection', () => {
+    this.server.on('connection', (socket: Socket) => {
       console.log('New client connected');
+
+      socket.on('disconnect', () => {
+        console.log('Cliente desconectado');
+      });
     });
+  }
+
+  @SubscribeMessage('send-message')
+  handleMessage(
+    @MessageBody() message: string,
+    @ConnectedSocket() client: Socket,
+  ) {
+    if (!message) return;
+
+    this.server.emit('on-message', { userId: client.id, message });
   }
 }
